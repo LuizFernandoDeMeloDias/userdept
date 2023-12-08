e144c6fa4e57a8ce262cdf53ed7ea4541a008a13
Luiz Dias <valitio@gmail.com>
2023-12-08 12:08:04 -0300

    Initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_departament(name) VALUES ('Gestão');
+INSERT INTO tb_departament(name) VALUES ('Informática');
+INSERT INTO tb_departament(name) VALUES ('Frente');
+
+INSERT INTO tb_user(departament_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
+INSERT INTO tb_user(departament_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
+INSERT INTO tb_user(departament_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
+INSERT INTO tb_user(departament_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
+INSERT INTO tb_user(departament_id, name, email) VALUES (3, 'Angelica', 'Agelica@gmail.com');
